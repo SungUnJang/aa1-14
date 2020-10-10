86d5f886af4d2020fff1d01ea6e502620f1f0094
SungUnJang <62781515+SungUnJang@users.noreply.github.com>
2020-10-10 22:54:46 +0900

    Add files via upload

---
+++ aa1-14-rpt02/start/hello_function.js
@@ -0,0 +1,5 @@
+function hello(what) {
+  console.log("Hello " + what + "!");
+}
+hello("aa14");
+hello("Redwoods, 장성운");
---
+++ aa1-14-rpt02/start/index_AA14.js
@@ -0,0 +1,6 @@
+// index_AA14.js
+
+var info = require("./AA14info.js");
+
+info("aa14", "JangSungUn", "010-1234-1234");
+info("aa14", "COMSI", "010-1234-124");
